921609b7f4a6e98baf3a749db744fc76dd44bfd4
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-10-06 16:18:04 +0200

    ajout de commentaire pour la documentation

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const btnClose = document.querySelector('#btnClose');
 
 function openModal() {
     //dans l'élément modal, utilise la fonction classList pour enlever la string displayNone de l'attribut de class
+    // On peut utiliser 'remove' a la place de toggle pour enlever la classe displayNone et afficher la modal
     modal.classList.toggle('displayNone');
 }
 //au click sur btnOpen, on lance la fonction openModal
 btnOpen.addEventListener('click', openModal);
 
+//création de la fonction closeModal
+//on peut utiliser 'add' a la place de toggle pour ajouter la classe displayNone et cacher la modal
 function closeModal() {
     modal.classList.toggle('displayNone');
 }
